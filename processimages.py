30e63d1e0da332f06f6f0eda0aef664a545394da
agngur <agngur@users.noreply.github.com>
2018-01-25 23:51:53 +0100

    updated align method, astrometry to fix still

--- processimages.py
+++ processimages.py
@@ -11,13 +11,28 @@ import astropy.io.fits as fits
 from astropy.time import Time
 from astropy import units as u
 from astropy.coordinates import SkyCoord
+import subprocess as sub
 from ccdproc import CCDData, Combiner, subtract_bias, subtract_dark, flat_correct
 import numpy as np
 import os
+from shutil import copyfile
 import alipy
 import ccdproc
-from astrometry_api_p3.client import Client
 import time 
+
+#-----------------------------------------------------#
+# solve-filed param
+time_limit = 15  # time limit before giving up
+scale_low = 32  # arcsec
+scale_high = 34  # arcsec
+solve_radius = 1  # deg
+#solve_depth = '40,80,100,160,250'
+
+# clear
+files_to_rm = ['*.axy', '*.corr', '*.xyls', '*.match', '*.rdls', '*.solved', '*.wcs']
+
+#-----------------------------------------------------#
+
 
 
 #im_dir = 'C:(...)'
@@ -106,7 +121,7 @@ def reduction_data(im_dir, exp_time, dark_exp, m_dark_name,
 
 def make_stack(images, save_dir, exp, filter_name, hdr_keys, ref_im_num=1):
     
-    
+    print('images', images)
     # CCD_data_table = list(map(CCDData.read(unit='adu'), images))
     CCD_data_table = [CCDData.read(im, unit='adu') for im in images]
     combiner = Combiner(CCD_data_table, dtype='float32')
@@ -133,6 +148,7 @@ def create_stack_hdr(filter_pack, hdr_keys):
     h1 = fits.getheader(filter_pack[0])
     h2 = fits.getheader(filter_pack[-1])
     master = fits.Header()
+
     for key in hdr_keys:
         if key == 'DATE-OBS':
             master['OBSSTART'] = h1[key]
@@ -156,20 +172,26 @@ def create_stack_hdr(filter_pack, hdr_keys):
                 master[key] = (h2[key] + h1[key]) / 2.
             except ValueError:
                 master[key] = h1[key]
+    master['IMNUM'] = len(filter_pack)
+
     return master
     
     
 def align_images(images, ref_im_num=0, save_dir=None, overwrite=True):
     
-    
     ref_image = images[ref_im_num]
-    identifications = alipy.ident.run(ref_image, images, visu=False, sexkeepcat=False, verbose=False)  
+    tempfile = os.path.join(os.path.dirname(ref_image), ".temp")
+    copyfile(ref_image, tempfile)
+    ref_image = tempfile
+   
+    identifications = alipy.ident.run(ref_image, images, visu=False, sexkeepcat=True, verbose=False)  
     #sex_command='sextractor'
 
     for id in identifications: 
         if id.ok == True: 
             print("{} : {}, flux ratio {}".format(id.ukn.name, id.trans, id.medfluxratio))
         else:
+            images.remove(id.ukn.filepath)
             print("{} : no transformation found !".format(id.ukn.name))
 
     outputshape = alipy.align.shape(ref_image)
@@ -179,31 +201,58 @@ def align_images(images, ref_im_num=0, save_dir=None, overwrite=True):
             alipy.align.affineremap(id.ukn.filepath, id.trans, shape=outputshape, makepng=False,
                                     overwrite=True)
 
+    return images            
             
-def make_astrometry(images):
-    server = 'http://nova.astrometry.net/api/'
-    apikey = 'abonxcbacdbfjbce'
-    client = Client(apiurl=server)
-    client.login(apikey)
-    #FIXME
-    im_ref = [im for im in images if 'P1' in im]
-    cat = im_ref[0].replace('_P1', '')
-    cat = cat.replace('.fit', '.astrometry')
-    wcs_file = cat.replace('.astrometry', '.wcs')
-    print('Cat ', cat)
-    print('WCS: ', wcs_file)
-    time.sleep(10)
-    cmd = 'python /home/agngur/polarimetric_reduction/astrometry_api_p3/client.py --server {server} --apikey {apikey} -u "{cat}" --wcs="{wcs_file}"'.format(server=server, apikey=apikey,
-                                             cat=cat, wcs_file=wcs_file)
-        
-    print(cmd)
-    os.system(cmd)
-    wcs = fits.getheader(wcs_file)
-    for im in images:
-        f = fits.open(im, mode='update')
-        f[0].header += wcs
-        f.flush()
-      
+def do_astrometry(cat_dir, limit=100):
+    cat_files = glob.glob(os.path.join(cat_dir,'*.pysexcat'))
+    for cat_file in cat_files:
+        cat = np.genfromtxt(cat_file, usecols=[0, 1, 2])
+        cat = cat[cat[:,2].argsort()[::-1]]
+        np.savetxt(cat_file, cat[:limit,:2], fmt="%.4f")
+
+    fit_files = glob.glob(os.path.join(cat_dir,'*.fit'))
+    for fit_file in fit_files:
+        fits_coo = open_file(fit_file)
+        solve_field(fits_coo, fit_file)
+
+def open_file(fit_file):
+    hdr = fits.getheader(fit_file)
+    print('fits coo:', str(hdr['OBJCTRA'])+" "+str(hdr['OBJCTDEC']))
+    fits_coo = SkyCoord(str(hdr['OBJCTRA'])+" "+str(hdr['OBJCTDEC']),
+                        'icrs', unit=(u.hour, u.deg))
+    return fits_coo
+
+def solve_field(fits_coo, fit_file):
+    solve_field_command = ['solve-field',
+                           '--ra', '%s' % (fits_coo.ra.deg),
+                           '--dec', '%s' % (fits_coo.dec.deg),
+                           '--radius', '%1.1f' % solve_radius,
+                           #'--depth', solve_depth,
+                           '--cpulimit', '%f' % time_limit,
+                           '--scale-units', 'arcsecperpix',
+                           '--scale-low', '%.5f' % scale_low,
+                           '--scale-high', '%.5f' % scale_high,
+                           '--overwrite',
+                           '--no-verify',
+                           '--no-plots',
+                           str(fit_file)]
+    sub.Popen(solve_field_command, stdout=sub.PIPE,
+              stderr=sub.PIPE).communicate()
+    if os.path.exists(str(fit_file).split(".")[0]+'.new'):
+        return True
+    else:
+        print('solve error')
+        return False
+    clear()
+
+def clear():
+    print('cleaning.....')
+    for i in files_to_rm:
+        files = glob.glob(path + i)
+        print(path + i)
+        for j in files:
+            os.remove(j)
+
     
 #create_MasDark(im_dir, 60)
 #create_MasDark(im_dir, 5)        
@@ -212,3 +261,4 @@ def make_astrometry(images):
 #reduction_data(im_dir, 60, 60, 'MDark60.fit')
 #reduction_data(im_dir, 5, 5, 'MDark5.fit')
 
+#solve_field(open_file(fits_file), fits_file)
